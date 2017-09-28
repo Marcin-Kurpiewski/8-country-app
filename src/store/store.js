ddeb3b9aa7870404c3cc94655d8d837974abc716
Marcin-Kurpiewski <kurus.mk@gmail.com>
2017-09-28 22:26:42 +0200

    add dispatch, didnt work

--- src/store/store.js
+++ src/store/store.js
@@ -2,11 +2,14 @@ import { createStore } from 'redux';
 import reducers from '../reducers/index';
 import DevTools from '../DevTools';
 
+
 const store = createStore(
     reducers,
     DevTools.instrument()
 );
 
+store.subscribe(() => console.log(store.getState())); // nasłuchiwanie na zmiany
 
+store.dispatch(getCountries('pierwsza-zmiana')); // wywołanie dispatch nie wprowadza zadnych zmian hm...
 
 export default store;
